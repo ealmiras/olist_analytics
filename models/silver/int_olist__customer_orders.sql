d3e0d043c031e5ee23d56d6155a3741f9187039b
Almira <ealmirasozlu@gmail.com>
2026-06-09 17:25:12 +0200

    initialize

---
+++ models/silver/int_olist__customer_orders.sql
@@ -0,0 +1,27 @@
+with 
+orders as (
+    select * from {{ ref('stg_olist__orders') }}
+),
+customers as (
+    select * from {{ ref('stg_olist__customers') }}
+),
+
+joined as (
+    select
+        o.order_id,
+        o.customer_id,
+        c.customer_unique_id,
+        c.customer_zip_code_prefix,
+        c.customer_city,
+        c.customer_state,
+        o.order_status,
+        o.purchased_at,
+        o.approved_at,
+        o.delivered_carrier_at,
+        o.delivered_customer_at,
+        o.estimated_delivery_at
+    from orders o
+    left join customers c on o.customer_id = c.customer_id
+)
+
+select * from joined
