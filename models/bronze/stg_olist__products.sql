9ca462043ff66f7c735dd5a61191a0c23e42214c
Almira <ealmirasozlu@gmail.com>
2026-06-18 12:50:32 +0200

    trasnlation seed added

--- models/bronze/stg_olist__products.sql
+++ models/bronze/stg_olist__products.sql
@@ -1,19 +1,24 @@
 with products as (
     select * from {{ source('olist', 'products') }}
 ),
+translations as (
+    select * from {{ ref('product_category_translations') }}
+),
 
 renamed as (
     select
-        product_id,
-        product_category_name,
-        product_name_length,
-        product_description_length,
-        product_photos_qty,
-        product_weight_g,
-        product_length_cm,
-        product_height_cm,
-        product_width_cm
-    from products
+        p.product_id,
+        p.product_category_name,
+        t.product_category_name_english,
+        p.product_name_length,
+        p.product_description_length,
+        p.product_photos_qty,
+        p.product_weight_g,
+        p.product_length_cm,
+        p.product_height_cm,
+        p.product_width_cm
+    from products p
+    left join translations t on t.product_category_name = p.product_category_name
 )
 
 select * from renamed
